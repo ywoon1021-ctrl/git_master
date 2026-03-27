0b3b4af88f898c620b6d84dc9c36154421957d48
youngwoon <ywoon1021@gmil.com>
2026-03-27 13:47:09 +0900

    [add] Clean up

--- src/main/java/com/yw/db/gitmaster/HelloServlet.java
+++ src/main/java/com/yw/db/gitmaster/HelloServlet.java
@@ -13,15 +13,14 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+
     }
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+    }
     public void destroy() {
     }
 }
